0ef58afa3ddea11cdf4341dbbe46e721ef0981ec
Sandeep Panda <spanda@codeaurora.org>
2016-02-11 10:44:19 +0530

    msm: mdss: do not re-configure dsi phy regulator for split mode
    In current implementation for split dsi mode when we configure
    dsi phy regulator for DSI1 controller, then it affects the phy
    regulator setting of DSI0 controller, since phy regulator is
    shared among both the controllers. This is causing abnormal
    wave form for DSI0 lanes. Fix this issue by checking for the
    state of phy regulator of the other controller, while configuring
    dsi phy regulator of one DSI controller.
    
    Change-Id: I318ae88d68f0acfa3e590ab07e5b2830aa4ae967
    Signed-off-by: Sandeep Panda <spanda@codeaurora.org>

--- drivers/video/msm/mdss/msm_mdss_io_8974.c
+++ drivers/video/msm/mdss/msm_mdss_io_8974.c
@@ -387,8 +387,6 @@ static void mdss_dsi_phy_regulator_ctrl(struct mdss_dsi_ctrl_pdata *ctrl,
 			 * other dsi controller is still active.
 			 */
 			if (mdss_dsi_is_hw_config_single(sdata) ||
-				(mdss_dsi_is_ctrl_clk_master(ctrl) &&
-					pinfo->ulps_suspend_enabled) ||
 				(other_ctrl && !other_ctrl->is_phyreg_enabled))
 				mdss_dsi_28nm_phy_regulator_enable(ctrl);
 		}
